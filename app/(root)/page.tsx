5fd2bc08cf33465c03098e3a773042abcb3d4b7c
sam esq <oladimos@gmail.com>
2024-08-01 06:48:18 -0600

    implement initial file and folder structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,26 @@
+import HeaderBox from "@/components/ui/HeaderBox"
+
+const Home = () => {
+  const loggedIn = { firstName: 'Benjamin' };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          
+          />
+
+        </header>
+
+
+      </div>
+      </section>
+  )
+}
+
+export default Home
